2a4e0728cf7386abb3c0bc76a56268673d094735
daduz11 <71891863+daduz11@users.noreply.github.com>
2020-09-26 15:58:08 +0200

    Update ml_converter.py

--- Python/ml_converter.py
+++ Python/ml_converter.py
@@ -1,4 +1,21 @@
 """
+   Copyright 2020 daduz11
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+
+"""
 Firstly this script is used for the conversion of the freezed inference graph (pb format) into a CoreML model.
 Moreover the same script takes the CoreML model at 32bit precision to carries out the quantization from 16 to 1 bit.
 """
